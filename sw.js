2b662ab3fbcc5a725c55080232a4d62b98caf907
Subin Viswanathan <subinv1992@gmail.com>
2024-07-04 14:22:02 +0530

    add the files

---
+++ sw.js
@@ -0,0 +1,15 @@
+self.addEventListener('install', (event) => {
+	console.log('[Service Worker] Installing Service Worker...', event);
+});
+
+self.addEventListener('activate', (event) => {
+	console.log('[Service Worker] Activating Service Worker...', event);
+
+	return self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+	console.log('[Service Worker] Fetching Somethig...', event);
+
+	event.respondWith(fetch(event.request));
+});
